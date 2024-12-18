baf00142bf105137fc2293b2ce56f5f1bff6a908
Jonathan Marques <jonathan.marques.costa@outlook.com>
2024-12-18 20:00:24 -0300

    route table

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "eks_subnet_public_1a" {
-  vpc_id                  = aws_vpc.eks_vpc.id
+  vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
   availability_zone       = "${data.aws_region.current.name}a"
   map_public_ip_on_launch = true
@@ -14,7 +14,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
 }
 
 resource "aws_subnet" "eks_subnet_public_1b" {
-  vpc_id                  = aws_vpc.eks_vpc.id
+  vpc_id                  = aws_vpc.this.id
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
   availability_zone       = "${data.aws_region.current.name}b"
   map_public_ip_on_launch = true
@@ -28,10 +28,20 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   )
 }
 
+resource "aws_route_table_association" "rtb_assoc_1a" {
+  subnet_id      = aws_subnet.eks_subnet_public_1a.id
+  route_table_id = aws_route_table.this.id
+}
+
+resource "aws_route_table_association" "rtb_assoc_1b" {
+  subnet_id      = aws_subnet.eks_subnet_public_1b.id
+  route_table_id = aws_route_table.this.id
+}
+
 
 #PRIVATE
 resource "aws_subnet" "eks_subnet_private_1a" {
-  vpc_id            = aws_vpc.eks_vpc.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
   availability_zone = "${data.aws_region.current.name}a"
 
@@ -45,7 +55,7 @@ resource "aws_subnet" "eks_subnet_private_1a" {
 }
 
 resource "aws_subnet" "eks_subnet_private_1b" {
-  vpc_id            = aws_vpc.eks_vpc.id
+  vpc_id            = aws_vpc.this.id
   cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
   availability_zone = "${data.aws_region.current.name}b"
 
--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "eks_vpc" {
+resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
