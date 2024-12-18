5c1753a28491700a55ed3cb098dc38791e2d0384
Jonathan Marques <jonathan.marques.costa@outlook.com>
2024-12-18 19:48:10 -0300

    subnets

--- subnet.tf
+++ subnet.tf
@@ -7,7 +7,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
   tags = merge(
     local.tags,
     {
-      Name                     = "devops_subnet_1a"
+      Name                     = "${var.project_name}-public-subnet-1a"
       "kubernetes.io/role/elb" = 1
     }
   )
@@ -22,8 +22,38 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   tags = merge(
     local.tags,
     {
-      Name                     = "devops_subnet_1b"
+      Name                     = "${var.project_name}-public-subnet-1b"
       "kubernetes.io/role/elb" = 1
+    }
+  )
+}
+
+
+#PRIVATE
+resource "aws_subnet" "eks_subnet_private_1a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3)
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.tags,
+    {
+      Name                              = "${var.project_name}-private-subnet-1a"
+      "kubernetes.io/role/internal-elb" = 1
+    }
+  )
+}
+
+resource "aws_subnet" "eks_subnet_private_1b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.tags,
+    {
+      Name                              = "${var.project_name}-private-subnet-1b"
+      "kubernetes.io/role/internal-elb" = 1
     }
   )
 }
--- variable.tf
+++ variable.tf
@@ -3,14 +3,7 @@ variable "cidr_block" {
   description = "Networking CIDR block to be used for the VPC"
 }
 
-variable "az_number" {
-  # Assign a number to each AZ letter used in our configuration
-  default = {
-    a = 1
-    b = 2
-    c = 3
-    d = 4
-    e = 5
-    f = 6
-  }
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to name the resources (Name tag)"
 }
