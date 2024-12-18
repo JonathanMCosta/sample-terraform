bb95d9d4d40cfc7616dd691247e34c7878311fa0
Jonathan Marques <jonathan.marques.costa@outlook.com>
2024-12-18 09:31:54 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.81.0"
     }
+  }
+  backend "s3" {
+    bucket = "project-devops-jotta"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
